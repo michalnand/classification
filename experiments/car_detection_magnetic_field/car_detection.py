d9892c2fd2f476f62a54eb9b95255cfe5d2e8488
michal.nand <michal.nand@gmail.com>
2020-08-30 05:31:56 +0200

    first results

--- experiments/car_detection_magnetic_field/car_detection.py
+++ experiments/car_detection_magnetic_field/car_detection.py
@@ -1,5 +1,5 @@
 import sys
-sys.path.insert(0,'..')
+sys.path.insert(0,'../..')
 
 import libs
 import libs_dataset
@@ -9,12 +9,14 @@ import models.magnetometer_net_2.model as Model2
 import models.magnetometer_net_3.model as Model3
 
 
+dataset_path = "/home/michal/dataset/"
 folders_list = []
-folders_list.append("/home/michal/dataset/car_detection_2/Meranie_20_06_03-Kinekus/1")
-folders_list.append("/home/michal/dataset/car_detection_2/Meranie_20_06_03-Kinekus/2")
-folders_list.append("/home/michal/dataset/car_detection_2/Porubka_03_06_2020")
-folders_list.append("/home/michal/dataset/car_detection_2/Meranie_20_05_22-Pribovce_xyz")
-folders_list.append("/home/michal/dataset/car_detection_2/Meranie_20_06_01-Lietavska_Lucka/01")
+
+folders_list.append(dataset_path + "/car_detection_2/Meranie_20_06_03-Kinekus/1")
+folders_list.append(dataset_path + "/car_detection_2/Meranie_20_06_03-Kinekus/2")
+folders_list.append(dataset_path + "/car_detection_2/Porubka_03_06_2020")
+folders_list.append(dataset_path + "/car_detection_2/Meranie_20_05_22-Pribovce_xyz")
+folders_list.append(dataset_path + "/car_detection_2/Meranie_20_06_01-Lietavska_Lucka/01")
 
 
 dataset = libs_dataset.DatasetMagnetometer2(folders_list, width = 512, augmentations_count = 10, testing_ratio = 0.2)
@@ -24,13 +26,13 @@ learning_rates  = [0.001, 0.0001, 0.0001, 0.0001, 0.00001, 0.00001]
 
 
 train = libs.Train(dataset, Model0, batch_size = 64, learning_rates = learning_rates)
-train.step_epochs(epoch_count, log_path = "../models/magnetometer_net_0")
+train.step_epochs(epoch_count, log_path = "../../models/magnetometer_net_0")
 
 train = libs.Train(dataset, Model1, batch_size = 64, learning_rates = learning_rates)
-train.step_epochs(epoch_count, log_path = "../models/magnetometer_net_1")
+train.step_epochs(epoch_count, log_path = "../../models/magnetometer_net_1")
 
 train = libs.Train(dataset, Model2, batch_size = 64, learning_rates = learning_rates)
-train.step_epochs(epoch_count, log_path = "../models/magnetometer_net_2")
+train.step_epochs(epoch_count, log_path = "../../models/magnetometer_net_2")
 
 train = libs.Train(dataset, Model3, batch_size = 64, learning_rates = learning_rates)
-train.step_epochs(epoch_count, log_path = "../models/magnetometer_net_3")
+train.step_epochs(epoch_count, log_path = "../../models/magnetometer_net_3")
