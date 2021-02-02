b3ab8638213a97bc137dfda3f3e2e1ef25c7291b
michalnand <michal.nand@gmail.com>
2021-02-02 19:45:57 +0100

    embedded net exports

--- experiments/mnist/run_training.py
+++ experiments/mnist/run_training.py
@@ -4,8 +4,8 @@ sys.path.insert(0,'../..')
 import libs
 import libs_dataset
 
-import models.model_0.model as Model0
-
+import models.model_1.model as Model1
+import models.model_2.model as Model2
 
 #train 30 epochs
 epoch_count     = 20 
@@ -13,8 +13,10 @@ epoch_count     = 20
 #use cyclic learning rate
 learning_rates  = [0.001, 0.0001, 0.0001]
 
-
 dataset = libs_dataset.DatasetMnist()
 
-train = libs.Train(dataset, Model0, libs.MetricsClassification, batch_size = 64, learning_rates = learning_rates)
-train.step_epochs(epoch_count, log_path = "./models/model_0")
+train = libs.Train(dataset, Model1, libs.MetricsClassification, batch_size = 64, learning_rates = learning_rates)
+train.step_epochs(epoch_count, log_path = "./models/model_1")
+
+train = libs.Train(dataset, Model2, libs.MetricsClassification, batch_size = 64, learning_rates = learning_rates)
+train.step_epochs(epoch_count, log_path = "./models/model_2")
