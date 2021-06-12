61e9e9a36ffa01b8cc3f1effff5410f6878f492b
michalnand <michal.nand@gmail.com>
2021-06-12 22:15:22 +0200

    common

--- experiments/transformations/run_training.py
+++ experiments/transformations/run_training.py
@@ -22,15 +22,15 @@ folders_training = []
 #folders_training.append("/Users/michal/dataset/outdoor/za_park/") 
 #folders_training.append("/Users/michal/dataset/outdoor/street/") 
 
-folders_training.append("/home/michal/dataset/outdoor/lietavska_lucka/")
-folders_training.append("/home/michal/dataset/outdoor/istrobotics_0/")
-folders_training.append("/home/michal/dataset/outdoor/istrobotics_1/")
-folders_training.append("/home/michal/dataset/outdoor/istrobotics_2/")
-folders_training.append("/home/michal/dataset/outdoor/nature/")
-folders_training.append("/home/michal/dataset/outdoor/za_park/")
-folders_training.append("/home/michal/dataset/outdoor/street/")
+folders_training.append("/home/michal/dataset/outdoor/lietavska_lucka/images/")
+folders_training.append("/home/michal/dataset/outdoor/istrobotics_0/images/")
+folders_training.append("/home/michal/dataset/outdoor/istrobotics_1/images/")
+folders_training.append("/home/michal/dataset/outdoor/istrobotics_2/images/")
+folders_training.append("/home/michal/dataset/outdoor/nature/images/")
+folders_training.append("/home/michal/dataset/outdoor/za_park/images/")
+folders_training.append("/home/michal/dataset/outdoor/street/images/")
  
-
+ 
 dataset = libs_dataset.DatasetTransformations(folders_training, folders_training, height = 256, width = 256)
 
 train = libs.Train(dataset, Model0, libs.MetricsRegression, batch_size = 32, learning_rates = learning_rates)
